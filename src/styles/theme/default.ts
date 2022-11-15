559900efeaafc2779c390e82ee606ea1cacbd53e
Cipriano99 <samuelcipriano99@gmail.com>
2022-11-14 21:38:46 -0300

    setup project 🎊

---
+++ src/styles/theme/default.ts
@@ -0,0 +1,16 @@
+export const defaultTheme = {
+  blue: '#3294f8',
+
+  'base-title': '#e7edf4',
+  'base-subtitle': '#c4d4e3',
+  'base-text': '#AFC2D4',
+
+  'base-span': '#7B96B2',
+  'base-label': '#3A536b',
+  'base-border': '#1c2f41',
+
+  'base-post': '#112131',
+  'base-profile': '#0b1b2b',
+  'base-background': '#071422',
+  'base-input': '#040f1a',
+}
